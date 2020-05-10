032be687220dc55a0f84db7a2ac77c74921e51e6
MichaelOreillyMor <michaeloreillym@gmail.com>
2020-05-10 19:48:56 +0200

    Pos-procesing finished

--- Assets/Scripts/Effects/ExplosionPlayer.cs
+++ Assets/Scripts/Effects/ExplosionPlayer.cs
@@ -27,26 +27,13 @@ namespace Asteroids.Effects
         [SerializeField]
         private AnimationCurve lensDistIntesity;
 
-        [SerializeField]
-        private AnimationCurve bloomIntesity;
-
         private ChromaticAberration chromaticAberration;
         private LensDistortion lensDistortion;
-        private Bloom bloom;
-
-        private float chromaticAberrationValue;
-        private float lensDistortionValue;
-        private float bloomValue;
 
         private void Awake()
         {
             postProcessVolume.profile.TryGet(out chromaticAberration);
             postProcessVolume.profile.TryGet(out lensDistortion);
-            postProcessVolume.profile.TryGet(out bloom);
-
-            chromaticAberrationValue = chromaticAberration.intensity.value;
-            lensDistortionValue = lensDistortion.intensity.value;
-            bloomValue = bloom.intensity.value;
         }
 
         protected override IEnumerator PlayFXs()
@@ -61,14 +48,11 @@ namespace Asteroids.Effects
             {
                 step = time / duration;
 
-                //chromaticAberration.intensity.value = chromaticAbIntesity.Evaluate(step);
-                //lensDistortion.intensity.value = lensDistIntesity.Evaluate(step);
-                //bloom.intensity.value = bloomIntesity.Evaluate(step);
-                bloom.intensity.Override(bloomIntesity.Evaluate(step));
+                chromaticAberration.intensity.Override(chromaticAbIntesity.Evaluate(step));
+                lensDistortion.intensity.Override(lensDistIntesity.Evaluate(step));
                 lightEmission.intensity = lightIntesity.Evaluate(step);
-                Debug.Log("step: " + step + " bloom: " + bloomIntesity.Evaluate(step));
+
                 time += Time.deltaTime;
- 
                 yield return null;
             }
 
